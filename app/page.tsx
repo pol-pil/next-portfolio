e321954e4a2d4de3a7bb25b3d1c1eafa10bfb312
pol-pil <polsapol555@gmail.com>
2026-07-10 17:35:36 +0800

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import ImageTrail from '@/components/image-trail'
 import { ResizableMain } from '@/components/resizable-main'
 import { ThemeProvider } from '@/components/theme-provider'
-// import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/react'
 import { TooltipProvider } from '@/components/ui/tooltip'
 
 function Home() {
    return (
       <ThemeProvider attribute='class' defaultTheme='dark' enableSystem>
-         {/* <Analytics /> */}
+         <Analytics />
          <div className='h-dvh w-full pt-4 px-2 sm:px-8 xl:px-36'>
             {/* ImageTrail behind everything */}
             <div className='fixed inset-0 z-0'>
